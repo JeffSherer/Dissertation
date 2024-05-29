237efdb3d80f3f1236af57a5ee70a7df871c605a
Jeffrey Sherer <lil.jeff@live.com>
2024-05-29 10:38:40 +0100

    add more memory to the slurm

--- llava_med_test.sh
+++ llava_med_test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=llava-med-test
-#SBATCH --output=llava-med-test.out
-#SBATCH --error=llava-med-test.err
+#SBATCH --output=/home/jjls2000/Dissertation/llava-med-test-%j.out
+#SBATCH --error=/home/jjls2000/Dissertation/llava-med-test-%j.err
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
+#SBATCH --mem=32G
 #SBATCH --time=02:00:00
 
 # Load necessary modules and activate environment
 flight env activate gridware
 module load apps/nvidia-cuda/11.2.2
+source ~/anaconda3/etc/profile.d/conda.sh
 conda activate llavamed
-
-
 
 # Run the test script
 python /users/jjls2000/LLaVA-Med/llava/eval/model_vqa.py \
@@ -22,5 +22,3 @@ python /users/jjls2000/LLaVA-Med/llava/eval/model_vqa.py \
     --image-folder /users/jjls2000/Dissertation/data/images \
     --answers-file /users/jjls2000/Dissertation/results/answer-file.jsonl \
     --temperature 0.0
-
-
