3e2e1623ffde38f7498a96aa282b356cd5f893bc
Jeffrey Sherer <lil.jeff@live.com>
2024-06-12 21:43:34 +0100

    update path slurm

--- llava_med_test.sh
+++ llava_med_test.sh
@@ -31,12 +31,12 @@ if [ ! -f $SCRIPT_PATH ]; then
 fi
 
 # Run the test script with SLaKE checkpoint
-python $SCRIPT_PATH \
+python /users/jjls2000/sharedscratch/LLaVA-Med/llava/eval/model_vqa.py \
     --conv-mode mistral_instruct \
     --model-path /users/jjls2000/sharedscratch/Dissertation/checkpoints/slake \
     --question-file /users/jjls2000/LLaVA-Med/data/eval/llava_med_eval_qa50_qa.jsonl \
     --image-folder /users/jjls2000/sharedscratch/Dissertation/data/images \
-    --answers-file ${RESULTS_DIR}/answer-file-${SLURM_JOB_ID}.jsonl \
+    --answers-file /users/jjls2000/sharedscratch/Dissertation/results/${EXPERIMENT_NAME}/answer-file-${SLURM_JOB_ID}.jsonl \
     --temperature 0.0
 
 # Optionally handle the results after experiment completes
