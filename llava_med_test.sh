1db652300ec941f8c78a50094b873b85fff71244
Jeffrey Sherer <lil.jeff@live.com>
2024-06-15 17:11:48 +0100

    slurm

--- llava_med_test.sh
+++ llava_med_test.sh
@@ -28,7 +28,7 @@ echo "Experiment Name: ${EXPERIMENT_NAME}"
 python /users/jjls2000/sharedscratch/LLaVA-Med/llava/eval/model_vqa.py \
     --conv-mode mistral_instruct \
     --model-path "microsoft/llava-med-v1.5-mistral-7b" \
-    --question-file "/users/jjls2000/LLaVA-Med/data/eval/llava_med_eval_qa50_qa.jsonl" \
+    --question-file /users/jjls2000/sharedscratch/LLaVA-Med/data/eval/llava_med_eval_qa50_qa.jsonl \
     --image-folder "/users/jjls2000/sharedscratch/Dissertation/data/images" \
     --answers-file "${RESULTS_DIR}/answer-file-${SLURM_JOB_ID}.jsonl" \
     --temperature 0.0
