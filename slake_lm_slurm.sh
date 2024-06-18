6e36dfdf4ede5474c8cb04bbd69f10ca9b4912bc
Jeffrey Sherer <lil.jeff@live.com>
2024-06-18 16:59:42 +0100

    Add split parts of imgs.zip using Git LFS

---
+++ slake_lm_slurm.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -l
+
+################# Part-1 Slurm directives ####################
+# Working dir
+#SBATCH -D /users/jjls2000/sharedscratch/Dissertation
+# Environment variables
+#SBATCH --export=ALL
+# Output and Error Files
+#SBATCH -o llava-med-test-%j.out
+#SBATCH -e llava-med-test-%j.err
+# Job name
+#SBATCH -J slake-test
+# Run time: "hours:minutes:seconds", "days-hours"
+#SBATCH --time=02:00:00
+# Memory limit (in gigabytes)
+#SBATCH --mem=32G
+# GPU requirements
+#SBATCH --gres=gpu:1
+# Specify partition
+#SBATCH -p gpu
+
+################# Part-2 Shell script ####################
+# Ensure the module system is ready and CUDA is available
+module load cuda/11.7  # Adjust as per your cluster's CUDA module availability
+
+# Source the conda initialization script
+source /opt/gridware/depots/761a7df9/el7/pkg/apps/anaconda3/2023.03/bin/etc/profile.d/conda.sh
+
+# Activate the conda environment
+conda activate llavamed
+
+# Check if model_vqa.py exists
+if [ ! -f /users/jjls2000/sharedscratch/LLaVA-Med/llava/eval/model_vqa.py ]; then
+  echo "Script model_vqa.py not found!"
+  exit 1
+fi
+
+# Verify the existence of necessary model files
+MODEL_PATH="/users/jjls2000/sharedscratch/Dissertation/Slake1.0-9epoch_delta"
+REQUIRED_FILES=("config.json" "pytorch_model.bin" "tokenizer_config.json" "tokenizer.model")
+
+for FILE in "${REQUIRED_FILES[@]}"; do
+  if [ ! -f "$MODEL_PATH/$FILE" ]; then
+    echo "Model file $FILE not found in $MODEL_PATH"
+    exit 1
+  fi
+done
+
+# Run the test script with SLaKE checkpoint
+python /users/jjls2000/sharedscratch/LLaVA-Med/llava/eval/model_vqa.py \
+    --conv-mode mistral_instruct \
+    --model-path $MODEL_PATH \
+    --question-file /users/jjls2000/sharedscratch/LLaVA-Med/data/eval/llava_med_eval_qa50_qa.jsonl \
+    --image-folder /users/jjls2000/sharedscratch/Dissertation/data/images \
+    --answers-file /users/jjls2000/sharedscratch/Dissertation/results/slake/answer-file-${SLURM_JOB_ID}.jsonl \
+    --temperature 0.0
