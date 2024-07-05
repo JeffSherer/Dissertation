452594e5eeb1fdf6eac96f5f512bfd648856d0d8
Jeffrey Sherer <lil.jeff@live.com>
2024-07-05 15:27:20 +0100

    slurm

--- BBF_finetune.sh
+++ BBF_finetune.sh
@@ -34,19 +34,25 @@ python -c "from transformers import LlamaConfig; print('LlamaConfig imported suc
 # Use the absolute path to the deepspeed in your Conda environment
 /users/jjls2000/.conda/envs/llavamed_new/bin/deepspeed /users/jjls2000/sharedscratch/Dissertation/llava/train/train_mem.py \
     --lora_enable True \
-    --deepspeed /users/jjls2000/sharedscratch/Dissertation/scripts/zero2.json \
-    --model_name_or_path "microsoft/llava-med-v1.5-mistral-7b" \
-    --version llava_med_v1.5 \
+    --lora_r 128 \
+    --lora_alpha 256 \
+    --mm_projector_lr 2e-5 \
+    --deepspeed /users/jjls2000/sharedscratch/Dissertation/scripts/zero3.json \
+    --model_name_or_path "lmsys/vicuna-13b-v1.5" \
+    --version v1 \
     --data_path "/users/jjls2000/sharedscratch/Dissertation/Slake1.0/augmented/BBF_train.json" \
     --image_folder "/users/jjls2000/sharedscratch/Dissertation/data/imgs-1" \
-    --vision_tower openai/clip-vit-large-patch14 \
+    --vision_tower openai/clip-vit-large-patch14-336 \
     --pretrain_mm_mlp_adapter "/users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-llava-llavammed-7b-pretrain/mm_projector.bin" \
+    --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
     --bf16 True \
     --output_dir "/users/jjls2000/sharedscratch/Dissertation/results/$(date +%Y%m%d_%H%M%S)" \
-    --num_train_epochs 3 \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
@@ -54,17 +60,17 @@ python -c "from transformers import LlamaConfig; print('LlamaConfig imported suc
     --save_strategy "steps" \
     --save_steps 50000 \
     --save_total_limit 1 \
-    --learning_rate 2e-5 \
+    --learning_rate 2e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
-    --logging_steps 100 \
+    --logging_steps 1 \
     --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --lazy_preprocess True \
     --dataloader_num_workers 4 \
-    --report_to none  # Change as per your tracking system, e.g., wandb
+    --lazy_preprocess True \
+    --report_to wandb
 
 echo "Training completed for BBF dataset."
 
