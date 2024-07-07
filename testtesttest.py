b1efb794a2d505f08e60d925afa75279dd5b5846
Jeffrey Sherer <lil.jeff@live.com>
2024-07-07 13:56:34 +0100

    test . py

---
+++ testtesttest.py
@@ -0,0 +1,8 @@
+# test_llava.py
+from llava.model.language_model.llava_llama import LlavaLlamaForCausalLM
+
+def test_llava():
+    print("LLaVA import successful!")
+
+if __name__ == "__main__":
+    test_llava()
