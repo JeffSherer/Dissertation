2a403469c749fecf7edbf698fb4ca7164230a88e
Jeffrey Sherer <lil.jeff@live.com>
2024-07-08 01:20:57 +0100

    slurm

--- BBF_finetune.sh
+++ BBF_finetune.sh
@@ -25,6 +25,14 @@ export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
 echo "CUDA_HOME is set to: $CUDA_HOME"
 nvcc --version
 
+# Create necessary directories if they don't exist
+mkdir -p /users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-llava-llavammed-7b-pretrain
+
+# Download the mm_projector.bin file if it doesn't already exist
+if [ ! -f /users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-llava-llavammed-7b-pretrain/mm_projector.bin ]; then
+    wget -O /users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-llava-llavammed-7b-pretrain/mm_projector.bin https://huggingface.co/liuhaotian/llava-v1.5-7b/resolve/main/mm_projector.bin
+fi
+
 ################# Part-3 Execute Fine-Tuning Script ####################
 # Use the absolute path to the deepspeed in your Conda environment
 /users/jjls2000/.conda/envs/llavamed_new/bin/deepspeed /users/jjls2000/sharedscratch/Dissertation/llava/train/train_mem.py \
