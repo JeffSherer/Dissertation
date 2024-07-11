7a7f874cb6a8655580a973e92adfa916ee4725db
Jeffrey Sherer <lil.jeff@live.com>
2024-07-11 22:47:19 +0100

    slurm

--- BBF_finetune.sh
+++ BBF_finetune.sh
@@ -1,31 +1,26 @@
 #!/bin/bash -l
 
 ################# Part-1 Slurm directives ####################
-## Working dir
+# Working dir
 #SBATCH -D /users/jjls2000/sharedscratch/Dissertation
-## Environment variables
+# Environment variables
 #SBATCH --export=ALL
-## Output and Error Files
+# Output and Error Files
 #SBATCH -o /users/jjls2000/sharedscratch/Dissertation/llava-med-test-%j.out
+# Error File
 #SBATCH -e /users/jjls2000/sharedscratch/Dissertation/llava-med-test-%j.err
-## Job name
+# Job name
 #SBATCH -J gpu-job
-## Run time: "hours:minutes:seconds", "days-hours"
+# Run time: "hours:minutes:seconds", "days-hours"
 #SBATCH --time=24:10:00
-## Memory limit (in megabytes)
+# Memory limit (in megabytes)
 #SBATCH --mem=32G
-## GPU requirements
+# GPU requirements
 #SBATCH --gres gpu:1
-## Specify partition
+# Specify partition
 #SBATCH -p gpu
 
 ################# Part-2 Shell script ####################
-#===============================
-#  Activate Flight Environment
-#-------------------------------
-source "${flight_ROOT:-/opt/flight}"/etc/setup.sh
-flight env activate gridware
-
 # Activate Conda environment
 source /users/jjls2000/.bashrc
 conda activate llavamed_new
@@ -46,11 +41,12 @@ deepspeed /users/jjls2000/sharedscratch/Dissertation/llava/train/train_mem.py \
     --lora_alpha 256 \
     --mm_projector_lr 2e-5 \
     --deepspeed /users/jjls2000/sharedscratch/Dissertation/scripts/zero3.json \
-    --model_name_or_path liuhaotian/llava-v1.5-13b \
-    --version v1 \
-    --data_path /users/jjls2000/sharedscratch/Dissertation/playground/data/llava_v1_5_mix665k.json \
-    --image_folder /users/jjls2000/sharedscratch/Dissertation/playground/data \
+    --model_name_or_path /users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-v1.5-7b \
+    --version llava_v1.5 \
+    --data_path /users/jjls2000/sharedscratch/Dissertation/Slake1.0/augmented/BBF_train.json \
+    --image_folder /users/jjls2000/sharedscratch/Dissertation/data/imgs-1 \
     --vision_tower openai/clip-vit-large-patch14-336 \
+    --pretrain_mm_mlp_adapter /users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-v1.5-7b/mm_projector_extracted/mm_projector/data.pkl \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -58,19 +54,19 @@ deepspeed /users/jjls2000/sharedscratch/Dissertation/llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir /users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-v1.5-13b-task-lora \
+    --output_dir /users/jjls2000/sharedscratch/Dissertation/results/20240709_214950 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
+    --evaluation_strategy no \
+    --save_strategy steps \
     --save_steps 50000 \
     --save_total_limit 1 \
     --learning_rate 2e-4 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
+    --lr_scheduler_type cosine \
     --logging_steps 1 \
     --tf32 True \
     --model_max_length 2048 \
