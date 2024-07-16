c11b23d7a994a2fa424c187774e7a28f6f31c061
Jeffrey Sherer <lil.jeff@live.com>
2024-07-16 16:49:30 +0100

    slurm

--- slake.sh
+++ slake.sh
@@ -48,43 +48,34 @@ nvidia-smi
 echo "Running on node(s): $SLURM_JOB_NODELIST"
 echo "Using GPU device(s): $CUDA_VISIBLE_DEVICES"
 
-# Run the training script with deepspeed
-deepspeed /users/jjls2000/sharedscratch/Dissertation/llava/train/train_mem.py \
-    --lora_enable True \
-    --lora_r 128 \
-    --lora_alpha 256 \
-    --mm_projector_lr 2e-5 \
-    --deepspeed /users/jjls2000/sharedscratch/Dissertation/scripts/zero3.json \
-    --model_name_or_path /users/jjls2000/sharedscratch/Dissertation/Slake1.0-9epoch_delta \
-    --version llava_v1.5 \
+# Run the training script with torchrun
+torchrun --nnodes=1 --nproc_per_node=8 --master_port=25001 \
+    /users/jjls2000/sharedscratch/Dissertation/llava/train/train_mem.py \
+    --model_name_or_path /users/jjls2000/sharedscratch/Dissertation/checkpoints/llava-med-7b-slake-delta \
     --data_path /users/jjls2000/sharedscratch/Dissertation/Slake1.0/augmented/BBF_train.json \
     --image_folder /users/jjls2000/sharedscratch/Dissertation/data/imgs-1 \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter /users/jjls2000/sharedscratch/Dissertation/Slake1.0-9epoch_delta/mm_projector_extracted/mm_projector/data.pkl \
-    --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
-    --group_by_modality_length True \
+    --mm_use_im_start_end True \
     --bf16 True \
     --output_dir /users/jjls2000/sharedscratch/Dissertation/results/20240709_214950 \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
-    --evaluation_strategy no \
-    --save_strategy steps \
-    --save_steps 50000 \
-    --save_total_limit 1 \
-    --learning_rate 2e-4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 5000 \
+    --save_total_limit 3 \
+    --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
-    --lr_scheduler_type cosine \
+    --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb
