e8a284f66c0155ec6d7f8a0de3a0feb769f7e286
Jeffrey Sherer <lil.jeff@live.com>
2024-07-17 12:05:17 +0100

    slurm

--- BBF_finetune.sh
+++ BBF_finetune.sh
@@ -26,7 +26,7 @@ source /users/jjls2000/.bashrc
 conda activate llava
 
 # Ensure CUDA paths are correct
-export CUDA_HOME=/opt/gridware/depots/761a7df9/el9/pkg/libs/nvidia-cuda/11.8.0/bin
+export CUDA_HOME=/opt/gridware/depots/761a7df9/el9/pkg/libs/nvidia-cuda/11.8.0
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:/users/jjls2000/.local/lib/python3.10/site-packages/nvidia/cudnn/lib:$LD_LIBRARY_PATH
 
