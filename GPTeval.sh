a41197329f79d47e42d38232a21683f89e4bbcf5
Jeffrey Sherer <lil.jeff@live.com>
2024-07-20 20:40:20 +0100

    slurm

--- GPTeval.sh
+++ GPTeval.sh
@@ -12,7 +12,13 @@
 source /opt/gridware/depots/761a7df9/el9/pkg/apps/anaconda3/2023.03/etc/profile.d/conda.sh
 conda activate llava-med
 
-# Set your OpenAI API key here
-export OPENAI_API_KEY="your-ope*******-key"
+# Ensure the OPENAI_API_KEY is set in your environment before running this script
+if [ -z "$OPENAI_API_KEY" ]; then
+  echo "ERROR: The environment variable OPENAI_API_KEY is not set."
+  exit 1
+fi
 
-python /users/jjls2000/sharedscratch/Dissertation/llava/eval/eval_multimodal_chat_gpt_score.py --answers-file /users/jjls2000/sharedscratch/Dissertation/results/llava_med_eval_answers.jsonl --question-file /users/jjls2000/sharedscratch/Dissertation/data/eval/llava_med_eval_qa50_qa.jsonl --scores-file /users/jjls2000/sharedscratch/Dissertation/results/eval_scores.jsonl
+python /users/jjls2000/sharedscratch/Dissertation/llava/eval/eval_multimodal_chat_gpt_score.py \
+  --answers-file /users/jjls2000/sharedscratch/Dissertation/results/llava_med_eval_answers.jsonl \
+  --question-file /users/jjls2000/sharedscratch/Dissertation/data/eval/llava_med_eval_qa50_qa.jsonl \
+  --scores-file /users/jjls2000/sharedscratch/Dissertation/results/eval_scores.jsonl
