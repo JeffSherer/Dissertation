74d7bb7cb1e4113adb18be5dc717a98efcfaf2a6
Jeffrey Sherer <lil.jeff@live.com>
2024-07-22 19:38:01 +0100

    deleting files

--- testtesttest.py
+++
@@ -1,5 +0,0 @@
-import torch
-from flash_attn.flash_attn_interface import flash_attn_func
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-print("Flash Attention and Transformers import successful!")
